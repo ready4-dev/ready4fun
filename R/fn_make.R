2bd7294e1bf5c86a400ddac8e54390d0defa3d05
Matthew P Hamilton <matthew.hamilton@orygen.org.au>
2020-09-15 15:43:47 +1000

    added pkg_ds_tb

--- R/fn_make.R
+++ R/fn_make.R
@@ -7,6 +7,7 @@
 #' @rdname make_arg_desc
 #' @export 
 
+#' @keywords internal
 make_arg_desc <- function (fn_args_chr, object_type_lup = NULL, abbreviations_lup = NULL) 
 {
     if (is.null(abbreviations_lup)) 
@@ -27,6 +28,7 @@ make_arg_desc <- function (fn_args_chr, object_type_lup = NULL, abbreviations_lu
 #' @export 
 #' @importFrom purrr map
 #' @importFrom stats setNames
+#' @keywords internal
 make_arg_desc_ls <- function (fn_nms_chr, abbreviations_lup = NULL, object_type_lup = NULL) 
 {
     if (is.null(abbreviations_lup)) 
@@ -48,6 +50,7 @@ make_arg_desc_ls <- function (fn_nms_chr, abbreviations_lup = NULL, object_type_
 #' @rdname make_arg_desc_spine
 #' @export 
 
+#' @keywords internal
 make_arg_desc_spine <- function (argument_nm_1L_chr, object_type_lup = NULL, abbreviations_lup = NULL) 
 {
     if (is.null(object_type_lup)) 
@@ -81,6 +84,7 @@ make_arg_desc_spine <- function (argument_nm_1L_chr, object_type_lup = NULL, abb
 #' @importFrom stringi stri_replace_last_fixed
 #' @importFrom stringr str_replace_all
 #' @importFrom Hmisc capitalize
+#' @keywords internal
 make_arg_title <- function (args_chr, match_chr, object_type_lup = NULL, abbreviations_lup = NULL) 
 {
     if (is.null(object_type_lup)) 
@@ -110,6 +114,7 @@ make_arg_title <- function (args_chr, match_chr, object_type_lup = NULL, abbrevi
 #' @export 
 #' @importFrom purrr map_chr discard pluck
 #' @importFrom rlang exec
+#' @keywords internal
 make_arg_type <- function (fn_args_chr, object_type_lup = NULL, abbreviations_lup = NULL, 
     fn) 
 {
@@ -142,6 +147,7 @@ make_arg_type <- function (fn_args_chr, object_type_lup = NULL, abbreviations_lu
 #' @rdname make_arg_type_abbr
 #' @export 
 
+#' @keywords internal
 make_arg_type_abbr <- function (fn_args_chr, object_type_lup = NULL, abbreviations_lup = NULL) 
 {
     if (is.null(abbreviations_lup)) 
@@ -160,6 +166,7 @@ make_arg_type_abbr <- function (fn_args_chr, object_type_lup = NULL, abbreviatio
 #' @rdname make_arg_type_abbr_spine
 #' @export 
 
+#' @keywords internal
 make_arg_type_abbr_spine <- function (argument_nm_1L_chr, lup_tb) 
 {
     arg_type_1L_chr <- lup_tb$short_name_chr[endsWith(argument_nm_1L_chr, 
@@ -176,6 +183,7 @@ make_arg_type_abbr_spine <- function (argument_nm_1L_chr, lup_tb)
 #' @export 
 #' @importFrom dplyr mutate filter
 #' @importFrom purrr map
+#' @keywords internal
 make_arg_type_lup_ls <- function (object_type_lup = NULL) 
 {
     if (is.null(object_type_lup)) 
@@ -233,6 +241,7 @@ make_dmt_for_all_fns <- function (paths_ls = make_fn_nms(), undocumented_fns_dir
 #' @export 
 #' @importFrom purrr pmap_chr
 #' @importFrom stringr str_extract
+#' @keywords internal
 make_fn_desc <- function (fns_chr, title_chr, output_chr, fn_type_lup_tb = NULL, 
     abbreviations_lup = NULL, test_for_write_R_warning_fn = NULL, 
     is_generic_lgl = F) 
@@ -273,6 +282,7 @@ make_fn_desc <- function (fns_chr, title_chr, output_chr, fn_type_lup_tb = NULL,
 #' @export 
 #' @importFrom purrr map_lgl map_chr
 #' @importFrom tools toTitleCase
+#' @keywords internal
 make_fn_desc_spine <- function (fn_name_1L_chr, fn_title_1L_chr, fn_type_lup_tb = NULL, 
     abbreviations_lup = NULL) 
 {
@@ -330,6 +340,7 @@ make_fn_desc_spine <- function (fn_name_1L_chr, fn_title_1L_chr, fn_type_lup_tb 
 #' @rdname make_fn_dmt_spine
 #' @export 
 
+#' @keywords internal
 make_fn_dmt_spine <- function (fn_name_1L_chr, fn_type_1L_chr, fn_title_1L_chr = NA_character_, 
     fn, details_1L_chr = NA_character_, example_1L_lgl = F, export_1L_lgl = T, 
     class_name_1L_chr, doc_in_class_1L_lgl) 
@@ -403,6 +414,7 @@ make_fn_dmt_tbl <- function (fns_path_chr, fns_dir_chr = make_undmtd_fns_dir_chr
 #' @importFrom tibble tibble
 #' @importFrom dplyr mutate filter
 #' @importFrom tools toTitleCase
+#' @keywords internal
 make_fn_dmt_tbl_tpl <- function (fns_path_chr, fns_dir_chr = make_undmtd_fns_dir_chr(), 
     fn_type_lup_tb = NULL, abbreviations_lup = NULL, object_type_lup = NULL, 
     test_for_write_R_warning_fn = NULL) 
@@ -444,6 +456,7 @@ make_fn_dmt_tbl_tpl <- function (fns_path_chr, fns_dir_chr = make_undmtd_fns_dir
 #' @export 
 #' @importFrom purrr map discard
 #' @importFrom stats setNames
+#' @keywords internal
 make_fn_nms <- function (path_1L_chr = "data-raw") 
 {
     fns_1L_chr_ls <- make_undmtd_fns_dir_chr(path_1L_chr) %>% 
@@ -465,6 +478,7 @@ make_fn_nms <- function (path_1L_chr = "data-raw")
 #' @importFrom Hmisc capitalize
 #' @importFrom purrr map_chr
 #' @importFrom stringi stri_replace_last_fixed
+#' @keywords internal
 make_fn_title <- function (fns_chr, object_type_lup = NULL, abbreviations_lup = NULL, 
     is_generic_lgl = F) 
 {
@@ -509,6 +523,7 @@ make_fn_type_lup <- function (fn_type_nm_chr = character(0), fn_type_desc_chr = 
 #' @rdname make_fn_types
 #' @export 
 
+#' @keywords internal
 make_fn_types <- function () 
 {
     fns_type_chr <- c("fns", "gnrcs", "mthds")
@@ -525,6 +540,7 @@ make_fn_types <- function ()
 #' @rdname make_gtr_str_dmt_spine
 #' @export 
 #' @importFrom stringr str_replace str_sub
+#' @keywords internal
 make_gtr_str_dmt_spine <- function (fn_type_1L_chr, fn_name_1L_chr, class_name_1L_chr, 
     doc_in_class_1L_lgl, example_1L_lgl = F) 
 {
@@ -624,6 +640,7 @@ make_lines_for_fn_dmt <- function (fn_name_1L_chr, fn_type_1L_chr, fn = NULL, fn
 #' @importFrom stringi stri_locate_last_fixed
 #' @importFrom purrr flatten_chr
 #' @importFrom stats setNames
+#' @keywords internal
 make_new_fn_dmt <- function (fn_type_1L_chr, fn_name_1L_chr, fn_desc_1L_chr = NA_character_, 
     fn_det_1L_chr = NA_character_, fn_out_type_1L_chr = NA_character_, 
     args_ls = NULL, fn = NULL, abbreviations_lup = NULL, object_type_lup = NULL) 
@@ -774,6 +791,7 @@ make_new_fn_dmt <- function (fn_type_1L_chr, fn_name_1L_chr, fn_desc_1L_chr = NA
 #' @importFrom dplyr bind_rows mutate filter select
 #' @importFrom purrr map2_chr map_chr
 #' @importFrom stringr str_sub str_replace
+#' @keywords internal
 make_obj_lup <- function () 
 {
     obj_tb <- tibble::tibble(short_name_chr = c("df", "fn", "ls", 
@@ -819,6 +837,7 @@ make_obj_lup <- function ()
 #' @rdname make_ret_obj_desc
 #' @export 
 
+#' @keywords internal
 make_ret_obj_desc <- function (fn, abbreviations_lup, starts_sentence_1L_lgl = T) 
 {
     ret_obj_nm_1L_chr <- get_return_obj_nm(fn)
@@ -845,6 +864,7 @@ make_ret_obj_desc <- function (fn, abbreviations_lup, starts_sentence_1L_lgl = T
 #' @rdname make_short_long_nms_vec
 #' @export 
 
+#' @keywords internal
 make_short_long_nms_vec <- function (long_vecs_chr = character(0), short_vecs_chr = character(0)) 
 {
     short_vecs_chr <- paste0(short_vecs_chr, "_vec")
@@ -871,6 +891,7 @@ make_short_long_nms_vec <- function (long_vecs_chr = character(0), short_vecs_ch
 #' @importFrom sinew makeOxygen
 #' @importFrom stringr str_replace
 #' @importFrom purrr discard
+#' @keywords internal
 make_std_fn_dmt_spine <- function (fn_name_1L_chr, fn_type_1L_chr, fn_title_1L_chr, fn, 
     details_1L_chr = NA_character_, example_1L_lgl = F, export_1L_lgl = T, 
     class_name_1L_chr = "", exclude_if_match_chr) 
@@ -918,6 +939,7 @@ make_std_fn_dmt_spine <- function (fn_name_1L_chr, fn_type_1L_chr, fn_title_1L_c
 #' @rdname make_undmtd_fns_dir_chr
 #' @export 
 
+#' @keywords internal
 make_undmtd_fns_dir_chr <- function (path_1L_chr = "data-raw") 
 {
     undocumented_fns_dir_chr <- paste0(path_1L_chr, "/", make_fn_types())

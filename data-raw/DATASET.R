47e7f4e48d12e9aaa68a666d46e4282a63dd8c6f
Matthew P Hamilton <matthew.hamilton@orygen.org.au>
2020-10-08 12:36:29 +1100

    Increment version number

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -174,8 +174,6 @@ write_links_for_website(user_manual_url_1L_chr = "https://readyforwhatsnext.gith
 # 11. Create vignettes
 # usethis::use_vignette("ready4fun")
 # devtools::document()
-
-
 # NOTE TO SELF: Currently Vignettes are overwritten by this last step. Need to implement more sophisticated workflow.
 # NOTE TO SELF: NEED TO RENAME export_lgl in tables and initial (not subsequent) functions to something like: inc_in_user_dmt_lgl
 # NOTE TO SELF: NEED TO ADD WORKFLOW FOR TRANSITIONING FROM PRIVATE TO PUBLIC REPO TO CLENSE ALL PRIVATE COMMIT HISTORY. Variant of: https://gist.github.com/stephenhardy/5470814

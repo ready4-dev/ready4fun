0a0df8424cf19dd080870e287419314e19e32f37
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2020-11-23 16:11:12 +1100

    Misc changes to pass R Cmd Check

---
+++ R/db_badges_lup.R
@@ -0,0 +1,13 @@
+#' ready4 badges lookup table
+#' 
+#' A lookup table to identify the appropriate text to insert in README files to represent different types of ready4 badges.
+#' 
+#' A tibble
+#' 
+#' \describe{
+#'   \item{names_chr}{Names (a character vector)}
+#'   \item{colours_chr}{Colours (a character vector)}
+#'   \item{badges_chr}{Badges (a character vector)}
+#' }
+#' @source \url{https://readyforwhatsnext.github.io/readyforwhatsnext/}
+"badges_lup"

53df40726b3f850e42f2ec000ef75aabe21dfd9d
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2020-12-04 17:03:02 +1100

    fix of add_lups bug

---
+++ R/db_abbreviations_lup.R
@@ -0,0 +1,13 @@
+#' Common abbreviations lookup table
+#' 
+#' A lookup table for abbreviations commonly used in object names in the ready4funpackage.
+#' 
+#' A tibble
+#' 
+#' \describe{
+#'   \item{short_name_chr}{Short name (a character vector)}
+#'   \item{long_name_chr}{Long name (a character vector)}
+#'   \item{plural_lgl}{Plural (a logical vector)}
+#' }
+#' @source \url{https://ready4-dev.github.io/ready4/}
+"abbreviations_lup"

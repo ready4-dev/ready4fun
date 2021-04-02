eac76211cd5f11f1d70faa768dd99a1ea2a25a9e
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2021-04-02 11:41:46 +1100

    another attempt at updated links

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -205,7 +205,6 @@ write_and_doc_fn_fls(fns_dmt_tb,
                      dev_pkgs_chr = NA_character_,
                      update_pkgdown_1L_lgl = T)
 #
-
 # write_links_for_website(user_manual_url_1L_chr = "https://github.com/ready4-dev/ready4fun/releases/download/v0.0.0.9279/ready4fun_user_0.0.0.9279.pdf",
 #                         developer_manual_url_1L_chr = "https://github.com/ready4-dev/ready4fun/releases/download/v0.0.0.9279/ready4fun_developer_0.0.0.9279.pdf",
 #                         project_website_url_1L_chr = "https://www.ready4-dev.com/")

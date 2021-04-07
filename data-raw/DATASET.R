ba48c3838b43323cff20d80de402770199271e76
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2021-04-07 14:09:11 +1000

    added links

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -144,8 +144,8 @@ write_and_doc_fn_fls(fns_dmt_tb,
 #
 # PAUSE FOR INTERACTIVE
 #
-write_links_for_website(user_manual_url_1L_chr = "https://github.com/ready4-dev/ready4fun/releases/download/v0.0.0.9279/ready4fun_user_0.0.0.9279.pdf",
-                        developer_manual_url_1L_chr = "https://github.com/ready4-dev/ready4fun/releases/download/v0.0.0.9279/ready4fun_developer_0.0.0.9279.pdf",
+write_links_for_website(user_manual_url_1L_chr = "https://github.com/ready4-dev/ready4fun/releases/download/v0.0.0.9289/ready4fun_user_0.0.0.9289.pdf",
+                        developer_manual_url_1L_chr = "https://github.com/ready4-dev/ready4fun/releases/download/v0.0.0.9289/ready4fun_developer_0.0.0.9289.pdf",
                         project_website_url_1L_chr = "https://www.ready4-dev.com/")
 
 # 12. Create vignettes

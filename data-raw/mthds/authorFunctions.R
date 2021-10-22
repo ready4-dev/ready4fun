dd4dc0198f35abd311ee310b9a89e40f0e7c6b68
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2021-10-23 09:04:59 +1100

    modified s3 mthd call

--- data-raw/mthds/authorFunctions.R
+++ data-raw/mthds/authorFunctions.R
@@ -1,5 +1,5 @@
 authorFunctions.ready4fun_manifest <- function(x,
-                                               list_generics_1L_lgl = F){
+                                               list_generics_1L_lgl = T){
   write_and_doc_fn_fls(x,
                        update_pkgdown_1L_lgl = T,
                        list_generics_1L_lgl = list_generics_1L_lgl)
